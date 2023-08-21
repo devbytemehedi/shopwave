d1eff1e0effd4cebc83089182540ead361ee3d5c
Mehedi Hasan <devbytemehedi@gmail.com>
2023-08-21 09:14:19 +0600

    ✅ ✨ 🐛 🔥 ⚡️ 🎨

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
       },
+      colors: {
+        grayish: "rgba(17, 17, 17, 0.05)",
+        main: "#E527B2",
+        blackish: "#111111",
+        demain: "#b31589",
+        whitish: "#F8F8F8",
+      },
     },
   },
   plugins: [require("daisyui")],
